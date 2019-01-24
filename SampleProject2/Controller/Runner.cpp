30cc671967ea042bedf7f393bcae3cafdc946f96
Eden Chambers <EdenChambers5@gmail.com>
2019-01-24 12:30:02 -0700

    Made project, deleted main.cpp, added controller group and runner.

---
+++ SampleProject2/Controller/Runner.cpp
@@ -0,0 +1,17 @@
+//
+//  Runner.cpp
+//  SampleProject2
+//
+//  Created by Chambers, Eden on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+//#include <stdio.h> //The C-Style Io library
+#include <iostream>
+
+int main()
+{
+    
+    
+    return 0;
+}
