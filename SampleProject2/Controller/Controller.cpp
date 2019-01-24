8c147b12eca7e5856aec3c72692de5dfa2a716a9
Eden Chambers <EdenChambers5@gmail.com>
2019-01-24 12:34:00 -0700

    Made controller with header file.
    Class is a type

---
+++ SampleProject2/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  SampleProject2
+//
+//  Created by Chambers, Eden on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ SampleProject2/Controller/Controller.hpp
@@ -0,0 +1,22 @@
+//
+//  Controller.hpp
+//  SampleProject2
+//
+//  Created by Chambers, Eden on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream> //doesn't know keywords yet
+using namespace std; //to make it know the keywords
+//class writing must be bewteen #define and #endif
+class Controller
+{
+private:
+public:
+};
+
+
+#endif /* Controller_hpp */
